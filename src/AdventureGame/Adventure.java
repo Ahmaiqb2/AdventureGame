52833b622449e4dd528e312b2f3554784ae4ec67
Ahmaiqb <Ahsa0084@stud.kea.dk>
2021-09-24 11:21:08 +0200

    Ændring i main, navne

--- src/AdventureGame/Adventure.java
+++ src/AdventureGame/Adventure.java
@@ -4,32 +4,31 @@ import java.util.Scanner;
 
 public class Adventure {
     public static void main(String[] args) {
-        Room direction = new Room("Lampe 1");
-        Room lampe2 = new Room("Lampe 2");
-        Room lampe3 = new Room("Lampe 3");
-        Room lampe4 = new Room("Lampe 4");
-        Room lampe5 = new Room("Lampe 5");
+        Room north = new Room("north");
+        Room south = new Room("south");
+        Room east = new Room("east");
+        Room west = new Room("west");
+
         Room selectedDirection = null;
 
         Scanner input = new Scanner(System.in);
 
         while(true) {
-            System.out.println(direction);
-            System.out.println(lampe2);
-            System.out.println(lampe3);
-            System.out.println(lampe4);
-            System.out.println(lampe5);
+            System.out.println(north);
+            System.out.println(south);
+            System.out.println(east);
+            System.out.println(west);
 
             System.out.print("\n Hvilken vej vil du? ");
             String lampe = input.nextLine();
 
 
             if ( lampe == "go north"){
-                selectedDirection = direction;
+                selectedDirection = north;
             } else if (lampe == "go south"){
-                selectedDirection = lampe2;
+                selectedDirection = south;
             } else if (lampe == "go west"){
-                selectedDirection = lampe3;
+                selectedDirection = east;
             } else if (lampe == "go east"){
                 selectedDirection = lampe4;
             }
