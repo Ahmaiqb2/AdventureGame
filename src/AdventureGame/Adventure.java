1e845a87dad266fff0fd06157f6217caa2171023
Battal <batt0006@stud.kea.dk>
2021-09-27 08:45:58 +0200

    Almost done

--- src/AdventureGame/Adventure.java
+++ src/AdventureGame/Adventure.java
@@ -3,20 +3,166 @@ package AdventureGame;
 import java.util.Scanner;
 
 public class Adventure {
+
+
+    Room[] rooms = new Room[9];
+
+
+    Room room1;
+    Room room2;
+    Room room3;
+    Room room4;
+    Room room5;
+    Room room6;
+    Room room7;
+    Room room8;
+    Room room9;
+
+
     public static void main(String[] args) {
+        Room[] rooms = new Room[9];
+        boolean running = true;
+
+        //Room room1;
+        //Room room2;
+        //Room room3;
+        /*Room room4;
+        Room room5;
+        Room room6;
+        Room room7;
+        Room room8;
+        Room room9;
+        */
+        /*null, room4, room2, null,
+                null, null, room3, room1, false,
+                null, room6, null, room2,
+                room1, room7, null, null,
+                null, room8, null, null,
+                room3, room9, null, null,
+
+                room4, null, room8, null
+
+        room5, null, room9, room7
+        room6, null, null, room8,*/
 
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Where do you wanna go?: ");
-        String inputUser = input.nextLine();
-        switch (inputUser) {
-            case "go north", "go n" -> System.out.println("go north");
-            case "go south", "go s" -> System.out.println("go south");
-            case "go east", "go e" -> System.out.println("go east");
-            case "go west", "go w" -> System.out.println("go west");
+        Room room1 = new Room("Room 1", true, new String[]{"There is a fridge", "There is a table"});
+        Room room2 = new Room("Room 2", false, new String[]{"There is a table with a pen and a safe"});
+        Room room3 = new Room("Room 3", false, new String[]{"There is a laptop, and a TV-screen connected to a Playstation 3"});
+        Room room4 = new Room("Room 4", false, new String[]{"There is a basketball, and some clothes"});
+        Room room5 = new Room("Room 5", false, new String[]{"There is some keys"});
+        Room room6 = new Room("Room 6", false, new String[]{"Theres is a table with some chips, and biscuits"});
+        Room room7 = new Room("Room 7", false, new String[]{"There is a teddy bear and some toys"});
+        Room room8 = new Room("Room 8", false, new String[]{"There is a table with a spreadsheet on it"});
+        Room room9 = new Room("Room 9", false, new String[]{"There is xx"});
 
+
+        room1.setDirections(null, room4, room2, null);
+        room2.setDirections(null, null, room3, room1);
+        room3.setDirections(null, room6, null, room2);
+        room4.setDirections(room1, room7, null, null);
+        room5.setDirections(null, room8, null, null);
+        room6.setDirections(room3, room9, null, null);
+        room7.setDirections(room4, null, room8, null);
+        room8.setDirections(room5, null, room9, room7);
+        room9.setDirections(room6, null, null, room8);
+
+        rooms[0] = room1;
+        rooms[1] = room2;
+        rooms[2] = room3;
+        rooms[3] = room4;
+        rooms[4] = room5;
+        rooms[5] = room6;
+        rooms[6] = room7;
+        rooms[7] = room8;
+        rooms[8] = room9;
+
+        while (running) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Where do you wanna go?: ");
+            String inputUser = input.nextLine();
+
+            System.out.println(inputUser);
+            if (inputUser.equals("go n") || inputUser.equals("go north")) {
+                System.out.println("Going north");
+
+
+                for (int i = 0; i < rooms.length; i++) {
+                    if (rooms[i].isCurrentRoom() == true) {
+                        if (rooms[i].getNorth() == null) {
+                            System.out.println("You can not go there");
+                            break;
+                        } else {
+                            System.out.println(rooms[i].getNorth());
+                            rooms[i].setCurrentRoom(false);
+                            rooms[i].getNorth().setCurrentRoom(true);
+                            System.out.println("You are now in " + rooms[i].getNorth().getName());
+                            break;
+
+                        }
+                    }
+
+                }
+            } else if (inputUser.equals("go s") || inputUser.equals("go south")) {
+                for (int i = 0; i < rooms.length; i++) {
+                    if (rooms[i].isCurrentRoom() == true) {
+
+                        if (rooms[i].getSouth() == null) {
+                            System.out.println("You can not go there");
+                            break;
+                        } else {
+
+                            rooms[i].setCurrentRoom(false);
+                            rooms[i].getSouth().setCurrentRoom(true);
+                            System.out.println("You are now in " + rooms[i].getSouth().getName());
+                            break;
+                        }
+                    }
+
+                }
+            } else if (inputUser.equals("go w") || inputUser.equals("go west")) {
+                for (int i = 0; i < rooms.length; i++) {
+                    if (rooms[i].isCurrentRoom() == true) {
+
+                        if (rooms[i].getWest() == null) {
+                            System.out.println("You can not go there");
+
+                        } else {
+                            rooms[i].setCurrentRoom(false);
+                            rooms[i].getWest().setCurrentRoom(true);
+                            System.out.println("You are now in " + rooms[i].getWest().getName());
+                            break;
+                        }
+                    }
+
+                }
+            } else if (inputUser.equals("go e") || inputUser.equals("go east")) {
+                for (int i = 0; i < rooms.length; i++) {
+                    if (rooms[i].isCurrentRoom() == true) {
+
+                        if (rooms[i].getEast() == null) {
+                            System.out.println("You can not go there");
+                            break;
+                        } else {
+                            rooms[i].setCurrentRoom(false);
+                            rooms[i].getEast().setCurrentRoom(true);
+                            System.out.println("You are now in " + rooms[i].getEast().getName());
+                            break;
+                        }
+                    }
+
+                }
+            } else if (inputUser.equals("look")) {
+                System.out.println();
+            }
         }
-
+        /*switch (inputUser) {
+            case "look" -> room1.look();
+            case "go north", "go n" -> System.out.println("Going north");
+            case "go south", "go s" -> System.out.println("Going south");
+            case "go east", "go e" -> System.out.println("Going east");
+            case "go west", "go w" -> System.out.println("Going west");
+        }*/
 
 
         /*Room north = new Room("north");
@@ -52,3 +198,4 @@ public class Adventure {
         }*/
     }
 }
+
--- src/AdventureGame/Room.java
+++ src/AdventureGame/Room.java
@@ -2,12 +2,95 @@ package AdventureGame;
 
 public class Room {
     private String name;
+    private String[] items = {};
+    private boolean currentRoom;
+    private Room north;
+    private Room south;
+    private Room east;
+    private Room west;
 
 
-    public Room(String name){
+    public Room(String name, boolean currentRoom, String[] items){ // Constructor - En constructor er en cunstructor når den har det samme navn som en klasse
+        this.name = name;
+        /*this.north = north;
+        this.south = south;
+        this.east = east;
+        this.west = west;*/
+        this.currentRoom = currentRoom;
+        this.items = items;
+
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
+    public void setItems(String[] items) {
+        this.items = items;
+    }
+
+    public void setCurrentRoom(boolean currentRoom) {
+        this.currentRoom = currentRoom;
+    }
+
+    public void setDirections(Room north, Room south, Room east, Room west) {
+        this.north = north;
+        this.south = south;
+        this.east = east;
+        this.west = west;
+    }
+
+    public void setNorth(Room north) {
+        this.north = north;
+    }
+
+    public void setSouth(Room south) {
+        this.south = south;
+    }
+
+    public void setEast(Room east) {
+        this.east = east;
+    }
+
+    public void setWest(Room west) {
+        this.west = west;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getItems() {
+        return items;
+    }
+
+    public boolean isCurrentRoom() {
+        return currentRoom;
+    }
+
+    public Room getNorth() {
+        return north;
+    }
+
+    public Room getSouth() {
+        return south;
+    }
+
+    public Room getEast() {
+        return east;
+    }
+
+    public Room getWest() {
+        return west;
+    }
+
+    public void look(){
+    System.out.println("You are currently in " + name);
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(items[i]);
+        }
+
+    }
     /*private boolean toggle;
     private String name;
 
