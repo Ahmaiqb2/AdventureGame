ffb313abc5d9d82a8ead53d506b68de6805bb4d8
Battal <batt0006@stud.kea.dk>
2021-09-30 21:32:02 +0200

    added item and player

---
+++ src/AdventureGame/Item.java
@@ -0,0 +1,15 @@
+package AdventureGame;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Item {
+
+private String itemName;
+private String itemDescription;
+
+public Item(String itemName, String itemDescription){
+this.itemName = itemName;
+this.itemDescription = itemDescription;
+}
+}
